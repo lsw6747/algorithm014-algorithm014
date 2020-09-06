7497e3b3948d9656bc62a8815af5aaba0c871f1d
lsw <lsw6747@163.com>
2020-09-06 22:03:53 +0800

    submit job

---
+++ Week_04/122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        int len = prices.length;
+        for (int i = 0; i < len - 1; i++) {
+            int diff = prices[i + 1] - prices[i];
+            if (diff > 0) {
+                res += diff;
+            }
+        }
+        return res;
+    }
+}
